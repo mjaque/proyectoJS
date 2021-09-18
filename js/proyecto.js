0de93ad6a0ff3d5d447fe19b475c22d28628e948
Miguel Jaque <mjaque@migueljaque.com>
2021-09-18 09:37:16 +0200

    Commit inicial

---
+++ js/proyecto.js
@@ -0,0 +1,12 @@
+/**
+ *   Descripción principal del fichero.
+ *   Descripción secundaria.
+ *   @author Miguel Jaque <mjaque@migueljaque.com>
+ *   @license GPL-3.0-or-later
+ *   Ref: https://spdx.org/licenses/
+ *
+ *   Ref JSDoc: https://jsdoc.app/
+ */
+'use strict'
+
+console.log('Cargado JS')
